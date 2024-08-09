c8cc6ba27ab81093a996adc1ca7096d443f981c1
Bohdan Shaban <bohdanshaban97@gmail.com>
2024-08-09 18:01:52 +0200

    RandomChar.js Component Work With API Functionality Was Craated...

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./components/app/App";
 import "./style/style.scss";
-import MarvelServise from "./api_services/MarvelService";
-
-const marvelServise = new MarvelServise();
-
-marvelServise.getAllCharacters().then(res => console.log(res));
 
 
 const container = document.getElementById("root");
